5cdd550cd0bc6b015b4a233a353c46aa44c094d7
Mathew Reiss <mathewreiss@Mathews-Mac.local>
2017-07-06 16:39:47 -0400

    Initial Commit

---
+++ Pulse v0/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pulse v0
+//
+//  Created by Mathew Reiss on 7/6/17.
+//  Copyright © 2017 Mathew Reiss. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
